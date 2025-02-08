8238f415d773635d57bc88d350d79c23b155e094
Tony Qu <tonyqug@gmail.com>
2025-02-07 19:32:56 -0500

    pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Home() {
   return (
     <div>
-      Testing deployment..
+      <div className="text-xl font-bold">Sta-tistics</div>
+      <div className="flex space-x-4">
+   
+        <Link href="/about" >Student</Link>
+        <Link href="/contact">Teacher</Link>
+      </div>
     </div>
   )
 }
