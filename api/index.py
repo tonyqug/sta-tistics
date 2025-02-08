53d85465d4ce70197518cdbefcb71c6af3336d59
LiamSchilling <wschilling003@gmail.com>
2025-02-07 22:26:58 -0500

    format functions, fetch by class

--- api/index.py
+++ api/index.py
@@ -21,27 +21,32 @@ def newStudentFeedback(className, studentName, slide, response):
 
 client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
+def insertStudentFeedback(data):
+    return client.from_("student_feedback").insert(data).execute()
+def insertStudentQuestions(data):
+    return client.from_("student_questions").insert(data).execute()
+def uploadPresentation(data):
+    return client.storage.from_("presentations").upload(data["name"], data["data"])
 def fetchStudentFeedback(className):
     return client.table("student_feedback").select("*").eq("class", className).execute()
+def fetchStudentQuestions(className):
+    return client.table("student_questions").select("*").eq("class", className).execute()
 
 @app.route('/api/insert-student-feedback', methods=['POST'])
 def insert_student_feedback():
     data = request.get_json()
-    res = client.from_("student_feedback").insert(data).execute()
-    print(res)
+    insertStudentFeedback(data)
 
 @app.route('/api/insert-student-question', methods=['POST'])
-def insert_student_questions():
+def insert_student_question():
     data = request.get_json()
     question = data["question"]
     response = "the-response"
     data["response"] = response
-    res = client.from_("student_questions").insert(data).execute()
-    print(res)
+    insertStudentQuestions(data)
     return response
 
-@app.route('/api/upload-pdf', methods=['POST'])
-def upload_pdf():
+@app.route('/api/upload-presentation', methods=['POST'])
+def upload_presentation():
     data = request.get_json()
-    res = client.storage.from_("presentations").upload(data["name"], data["data"])
-    print(res)
+    uploadPresentation(data)
