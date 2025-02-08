f1d059a60ee9ff01e3b9a374c99517e2d2c321c3
Tony Qu <tonyqug@gmail.com>
2025-02-07 21:52:05 -0500

    lecture

--- app/teacher/viewer.tsx
+++ app/teacher/viewer.tsx
@@ -9,6 +9,7 @@ pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
 export default function PdfViewer() {
   const [pdfData, setPdfData] = useState<any>(null);
   const [pageNumber, setPageNumber] = useState(1);
+  const [code, setCode] = useState("");
   const canvasRef = useRef(null);
 
   const handleFileChange = async (e: any) => {
@@ -47,26 +48,44 @@ export default function PdfViewer() {
   }, [pageNumber, pdfData]);
 
   return (
-    <div>
-      <input type="file" accept=".pdf" onChange={handleFileChange} />
-      {pdfData && (
-        <div>
-          <canvas ref={canvasRef}></canvas>
-          <div>
-            <button
-              onClick={() => setPageNumber((prev) => Math.max(prev - 1, 1))}
-            >
-              Previous
-            </button>
-            <span>Page {pageNumber}</span>
-            <button
-              onClick={() => setPageNumber((prev) => prev + 1)}
-            >
-              Next
-            </button>
-          </div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="p-6 rounded-lg w-full items-center flex flex-col justify-center">
+      <div className="mb-4">
+        <label htmlFor="file-upload" className="block w-[400px] shadow-lg whitespace-nowrap font-medium text-[20pt] text-gray-700 cursor-pointer bg-green-200 w-full p-2 rounded-lg text-center">
+            {pdfData ? `Class Code: ${code}`:"Upload Lecture PDF"}
+        </label>
+        <input
+            id="file-upload"
+            type="file"
+            accept=".pdf"
+            onChange={handleFileChange}
+            className="hidden"
+        />
+   
         </div>
-      )}
+
+
+        {pdfData && (
+          <div className="relative">
+            <canvas ref={canvasRef} className="h-full max-w-full border rounded-md"></canvas>
+            <div className="flex justify-between items-center mt-4">
+              <button
+                className="px-4 py-2 text-white bg-teal-500 rounded-md hover:bg-teal-600 transition duration-200"
+                onClick={() => setPageNumber((prev) => Math.max(prev - 1, 1))}
+              >
+                &#8592; Previous
+              </button>
+              <span className="text-xl font-semibold text-gray-800">Slide {pageNumber}</span>
+              <button
+                className="px-4 py-2 text-white bg-teal-500 rounded-md hover:bg-teal-600 transition duration-200"
+                onClick={() => setPageNumber((prev) => prev + 1)}
+              >
+                Next &#8594;
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
